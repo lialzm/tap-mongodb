246cb92ee3dfee1a574881e1aedb44081b9188e2
lialzm <6356434+lialzm@users.noreply.github.com>
2022-10-21 12:04:52 +0800

    修改字段类型

--- tap_mongodb/__init__.py
+++ tap_mongodb/__init__.py
@@ -168,7 +168,7 @@ def produce_collection_schema(collection):
                 },
                 "_sdc_deleted_at": {
                     "type": [
-                        "string",
+                        "timestamp without time zone",
                         "null"
                     ]
                 },
